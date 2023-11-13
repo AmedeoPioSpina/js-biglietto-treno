4b94c05358be72e3bd1eab0e876e5136925f2695
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2023-11-13 09:53:01 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -8,13 +8,11 @@ let costoChilometri = chilometri * 0.21;
 if( età < 18){
     costoReale = costoChilometri - (costoChilometri * 20 / 100);
 }
-else if (età > 65) {
+else if (età >= 65) {
     costoReale = costoChilometri - (costoChilometri * 40 / 100);
 }
 else{
     costoReale = costoChilometri;
 }
 
-costoReale = costoReale.toFixed(2);
-
-target.innerHTML = `Il biglietto costa: €${costoReale}`;
+target.innerHTML = `Il biglietto costa: €${costoReale.toFixed(2)}`;
